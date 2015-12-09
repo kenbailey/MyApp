4dc85e4ea0d44c68122892ccec372fcb91eac099
Ken Bailey <kenbailey@users.noreply.github.com>
2015-12-08 22:51:53 -0700

    updates

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Squirrel;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +16,29 @@ namespace MyApp
         [STAThread]
         static void Main()
         {
+
+           Task task = Task.Run(async () =>
+            {
+
+                try
+                {
+                    File.AppendAllText("C:\\temp\\log.txt", DateTime.Now.ToShortTimeString() + " Starting update...\r\n");
+
+                    await UpdateManager.GitHubUpdateManager("https://github.com/kenbailey/MyAppGitHub");
+                    File.AppendAllText("C:\\temp\\log.txt", DateTime.Now.ToShortTimeString() + " Finished Update\r\n");
+                }
+                catch (AggregateException aggEx)
+                {
+                    File.AppendAllText("C:\\temp\\log.txt", DateTime.Now.ToShortTimeString() + " Exception: " + aggEx.Message + "\r\n");
+                    foreach (Exception ex in aggEx.Flatten().InnerExceptions)
+                   { 
+                        File.AppendAllText("C:\\temp\\log.txt", DateTime.Now.ToShortTimeString() + " - " + ex.Message + "\r\n" + ex.StackTrace + "\r\n\r\n");
+                   }
+                }
+               
+            });
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
